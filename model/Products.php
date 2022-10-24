7b1c1fa92c0cbd59471951e01dc36ad288bfa631
arcelioM <arceliomonte15@gmail.com>
2022-10-24 08:30:02 -0500

    Entidad Products agregado

--- model/Products.php
+++ model/Products.php
@@ -4,14 +4,48 @@
   
   class Products {
 
-    private String $productoCode;
-    private String $productoName;
-    private String $productoLine;
+    private String $productCode;
+    private String $productName;
+    private String $productLine;
     private String $productScale;
     private String $productVendor;
     private String $productDescription;
     private int $quantityInStock;
     private float $buyPrice;
     private float $MSRP;
+
+    public function __construct(String $productCode, String $productName, String $productLine, String $productScale, String $productVendor, String $productDescription, int $quantityInStock, float $buyPrice, float $MSRP)
+    {
+      $this->productCode = $productCode;
+      $this->productName = $productName;
+      $this->productLine = $productLine;
+      $this->productScale = $productScale;
+      $this->productVendor = $productVendor;
+      $this->productDescription = $productDescription;
+      $this->quantityInStock = $quantityInStock;
+      $this->buyPrice = $buyPrice;
+      $this->MSRP = $MSRP;
+    }
+
+    public function __get($name)
+    {
+      if(property_exists($this,$name)){
+        return $this->$name;
+      }
+
+      return null;
+    }
+
+    public function __set($name, $value)
+    {
+      if(property_exists($this, $name)){
+        $this->$name = $value;
+      }
+    }
+
+    public function __toString()
+    {
+      return "PRODUCTCODE : $this->productCode | PRODUCTNAME : $this->productName | PRODUCTLINE : $this->productLine | PRODUCTSCALE : $this->productScale | PRODUCTVENDOR : $this->productVendor | QUANTITYINSTOCK : $this->quantityInStock | BUPRICE : $this->buyPrice | MSRP : $this->MSRP";
+    }
   }
 ?>
