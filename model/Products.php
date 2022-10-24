6c0a7a093db82e940c7e45fdd8b69c0051c6a89e
arcelioM <arceliomonte15@gmail.com>
2022-10-23 22:06:39 -0500

    Atributos de la entidad products agregada

---
+++ model/Products.php
@@ -0,0 +1,17 @@
+<?php
+
+  use model;
+  
+  class Products {
+
+    private String $productoCode;
+    private String $productoName;
+    private String $productoLine;
+    private String $productScale;
+    private String $productVendor;
+    private String $productDescription;
+    private int $quantityInStock;
+    private float $buyPrice;
+    private float $MSRP;
+  }
+?>
