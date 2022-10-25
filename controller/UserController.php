88037118b72395e7515f7e5ba7484a34edd5d781
arcelioM <arceliomonte15@gmail.com>
2022-10-24 19:54:51 -0500

    Controlador para user agregado

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,15 +1,15 @@
 <?php
-
+require_once("../autoloads.php");
 use dao\connection\Connection;
 use dao\user\UserDaoImpl;
 use service\user\ServiceUserImpl;
 
-    if($_REQUEST['METHOD']==="GET"){
+    if($_SERVER["REQUEST_METHOD"]==="GET"){
 
         $conexionBD= Connection::getInstance();
         $daoUser = new UserDaoImpl($conexionBD);
         $serviceUser = new ServiceUserImpl($daoUser);
-        $users = $serviceUser->getAll();
-        return json_encode($users);
+        $users =$serviceUser->getAll();
+        echo json_encode($users);
     }
 ?>
